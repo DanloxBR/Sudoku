4f1bcff591433f7174a3df299c2f1d79cb8650aa
DanloxBR <danlox0602@gmail.com>
2025-12-19 16:12:43 -0300

    Projeto Sudoku - versão inicial

--- src/main/java/com/bringto/sudoku/model/Space.java
+++ src/main/java/com/bringto/sudoku/model/Space.java
@@ -1,21 +1,35 @@
-package com.bringto.soduko.model;
+package com.bringto.sudoku.model;
 
 public class Space {
 
     private Integer actual;
     private final int expected;
     private final boolean fixed;
+    private final int row;
+    private final int col;
 
-    public Space(int expected, boolean fixed) {
+    private CellStatus status;
+
+    public Space(int expected, boolean fixed, int row, int col) {
         this.expected = expected;
         this.fixed = fixed;
+        this.row = row;
+        this.col = col;
+
         if (fixed) {
-            actual = expected;
+            lock();
         }
     }
 
-    public Integer getActual() {
-        return actual;
+    private void lock() {
+        this.actual = expected;
+        this.status = CellStatus.CORRECT;
+    }
+
+    public void clearSpace() {
+        if (fixed) return;
+        this.actual = null;
+        this.status = null;
     }
 
     public void setActual(Integer actual) {
@@ -23,15 +37,31 @@ public class Space {
         this.actual = actual;
     }
 
-    public void clearSpace() {
-        setActual(null);
+    public boolean isFixed() {
+        return fixed;
+    }
+
+    public Integer getActual() {
+        return actual;
     }
 
     public int getExpected() {
         return expected;
     }
 
-    public boolean isFixed() {
-        return fixed;
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public CellStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CellStatus status) {
+        this.status = status;
     }
 }
